cc590531c3e3f6234680d59f2ce86c2a3b5e9d6a
AfiqRosli <afiq_rosli@live.com>
2021-03-03 22:51:18 +0800

    computedDate in Date comp
    check if today or yesterday or others

--- src/components/Date.vue
+++ src/components/Date.vue
@@ -1,15 +1,38 @@
 <template>
-  <v-card class="mx-auto my-10" max-width="150" rounded="lg">
-    <v-card-title class="justify-center pa-1">{{ date }}</v-card-title>
+  <v-card class="mx-auto my-10" max-width="250" rounded="lg">
+    <v-card-title class="justify-center pa-1">{{ computedDate }}</v-card-title>
   </v-card>
 </template>
 
 <script>
+import dayjs from "dayjs";
+
 export default {
   props: {
     date: {
       type: String,
       required: true
+    }
+  },
+  data() {
+    return {
+      dayjs
+    };
+  },
+  computed: {
+    computedDate() {
+      let dateNow = dayjs(Date());
+      let dateProp = dayjs(this.date);
+
+      if (dateNow.diff(dateProp, "day") == 0) {
+        return "Today";
+      }
+
+      if (dateNow.diff(dateProp, "day") == 1) {
+        return "Yesterday";
+      }
+
+      return dateProp.format("MMMM D, YYYY");
     }
   }
 };
