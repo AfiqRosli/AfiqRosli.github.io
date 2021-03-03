157f6412e03ed01dab15e870fd03ed01108d18b9
AfiqRosli <afiq_rosli@live.com>
2021-03-03 22:22:12 +0800

    Selection comp init; response options
    e.g. cv, about, projects

---
+++ src/components/Selection.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card class="text-right py-3 selection">
+    <v-container>
+      <v-row>
+        <v-col offset="1" cols="10">
+          <v-btn class="mr-3">CV</v-btn>
+          <v-btn class="mr-3">About</v-btn>
+          <v-btn class="mr-10">Projects</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.selection {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+</style>
