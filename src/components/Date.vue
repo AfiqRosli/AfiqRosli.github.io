1ed210315fbd925fd759aca721034c7d78d468fa
AfiqRosli <afiq_rosli@live.com>
2021-03-03 22:19:29 +0800

    Date comp init for "Today" or "Yesterday" etc.

---
+++ src/components/Date.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card class="mx-auto my-10" max-width="150" rounded="lg">
+    <v-card-title class="justify-center pa-1">{{ date }}</v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    date: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
