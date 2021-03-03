7091e3c46fa36555faf3e6c5de23ad8ffd61d0e7
AfiqRosli <afiq_rosli@live.com>
2021-03-03 22:19:09 +0800

    ChatYou init derived from BaseChat

---
+++ src/components/ChatYou.vue
@@ -0,0 +1,30 @@
+<template>
+  <BaseChat justify="end" :msg="msg" :date="date">
+    <v-avatar class="mr-4" color="indigo">
+      <v-icon dark>
+        mdi-account-circle
+      </v-icon>
+    </v-avatar>
+    <p class="ma-0">You</p>
+  </BaseChat>
+</template>
+
+<script>
+import BaseChat from "@/components/BaseChat";
+
+export default {
+  components: { BaseChat },
+  props: {
+    msg: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
