28321521c86f02c3500942ae2bd0a4260c46f8fd
AfiqRosli <afiq_rosli@live.com>
2021-03-04 22:22:55 +0800

    talkOnAbout method

--- src/components/Selection.vue
+++ src/components/Selection.vue
@@ -4,6 +4,7 @@
       <v-row>
         <v-col offset="1" cols="10">
           <v-btn class="mr-5" @click="talkOnCV">CV</v-btn>
+          <v-btn class="mr-5" @click="talkOnAbout">About</v-btn>
           <v-btn class="mr-5">Projects</v-btn>
           <v-btn class="mr-10">
             <a
@@ -59,6 +60,58 @@ export default {
         });
       }, 750);
     },
+    async talkOnAbout() {
+      await this.addChat(
+        new ChatFactory(Chat.TEXT, {
+          person: Person.YOU,
+          msg: "About"
+        })
+      );
+
+      this.$vuetify.goTo(9999, {
+        duration: 700,
+        easing: "easeInCubic"
+      });
+
+      let count = 0;
+      let chatContent = [
+        "Hmm, where do I begin?",
+        "I recently finished my degree",
+        "Achieved Upper Second-Class Honours (2:1) in Computer Science",
+        "I see programming as my hobby and profession",
+        "(very thankful for that)",
+        "In technical 'about'",
+        "This website is done using Vue.js 2 & Vuetify",
+        "P.S. my response is fixed and do treat yourself with the Mystery Gift 🎁"
+      ];
+      await this.addChat(new ChatFactory(Chat.LOADING));
+
+      // repeat with the interval of 2.3 seconds
+      let chatTimer = setInterval(async () => {
+        await this.replaceLoading(
+          new ChatFactory(Chat.TEXT, {
+            person: Person.ME,
+            msg: chatContent[count++]
+          })
+        );
+
+        if (count < chatContent.length) {
+          await this.addChat(new ChatFactory(Chat.LOADING));
+        }
+
+        this.$vuetify.goTo(9999, {
+          duration: 700,
+          easing: "easeInCubic"
+        });
+      }, 2300);
+
+      // after 18.5 seconds stop; 2.3 * 8 + 100 = 18.5
+      // 2.3 * 8 is 8 chat content
+      // Add 100 millisecond to give time for the 9th chat appear
+      setTimeout(() => {
+        clearInterval(chatTimer);
+      }, 18500);
+    }
   }
 };
 </script>
