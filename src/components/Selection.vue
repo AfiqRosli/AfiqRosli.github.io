c46116544875b6ccd09fa36dffd15a63c7950abd
AfiqRosli <afiq_rosli@live.com>
2021-03-04 22:22:42 +0800

    talkOnCV method

--- src/components/Selection.vue
+++ src/components/Selection.vue
@@ -3,8 +3,7 @@
     <v-container>
       <v-row>
         <v-col offset="1" cols="10">
-          <v-btn class="mr-5">CV</v-btn>
-          <v-btn class="mr-5">About</v-btn>
+          <v-btn class="mr-5" @click="talkOnCV">CV</v-btn>
           <v-btn class="mr-5">Projects</v-btn>
           <v-btn class="mr-10">
             <a
@@ -30,6 +29,36 @@ import { mapActions } from "vuex";
 export default {
   methods: {
     ...mapActions(["addChat", "replaceLoading"]),
+    async talkOnCV() {
+      await this.addChat(
+        new ChatFactory(Chat.TEXT, {
+          person: Person.YOU,
+          msg: "CV"
+        })
+      );
+
+      this.$vuetify.goTo(9999, {
+        duration: 700,
+        easing: "easeInCubic"
+      });
+
+      await this.addChat(new ChatFactory(Chat.LOADING));
+
+      setTimeout(async () => {
+        await this.replaceLoading(
+          new ChatFactory(Chat.TEXT, {
+            person: Person.ME,
+            msg:
+              "<a href='/files/afiq_rosli_software_engineer_cv_2021-03-03.pdf' target='_blank' download>Here you go</a>"
+          })
+        );
+
+        this.$vuetify.goTo(9999, {
+          duration: 700,
+          easing: "easeInCubic"
+        });
+      }, 750);
+    },
   }
 };
 </script>
