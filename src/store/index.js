1711d683e75dfbf3d4c707f79fbf0b4c8b55261d
AfiqRosli <afiq_rosli@live.com>
2021-03-04 15:25:24 +0800

    adding chat to chats; mutation & action
    CREATE_CHAT, addChat

--- src/store/index.js
+++ src/store/index.js
@@ -7,4 +7,21 @@ export default new Vuex.Store({
   state: {
     chats: []
   },
+  mutations: {
+    CREATE_CHAT(state, chat) {
+      state.chats.push(chat);
+    },
+  },
+  actions: {
+    addChat({ commit }, chat) {
+      return new Promise((resolve, reject) => {
+        try {
+          commit("CREATE_CHAT", chat);
+          resolve();
+        } catch (error) {
+          reject(error);
+        }
+      });
+    },
+  }
 });
