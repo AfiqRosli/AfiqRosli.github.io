cb8afdadbdd786d0d2268d8c6f1756a89caed6b8
AfiqRosli <afiq_rosli@live.com>
2021-03-06 00:23:55 +0800

    ProjectCard init to showcasing the project in card

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-card min-width="300" max-width="400" class="mx-5 mb-10">
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      :src="require(`@/assets/projects/${src}`)"
+    >
+      <v-card-title class="text-shadow">{{ title }}</v-card-title>
+    </v-img>
+
+    <v-card-subtitle v-if="subtitle" class="pb-0">
+      {{ subtitle }}
+    </v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <p><slot></slot></p>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn v-if="link_website" color="primary" text>
+        <a class="text-decoration-none" :href="link_website" target="_blank"
+          >Website</a
+        >
+      </v-btn>
+
+      <v-btn v-if="link_github" color="primary" text>
+        <a class="text-decoration-none" :href="link_github" target="_blank"
+          >GitHub Repo</a
+        >
+      </v-btn>
+
+      <v-btn v-if="link_ig_post" color="primary" text>
+        <a class="text-decoration-none" :href="link_ig_post" target="_blank"
+          >IG Post</a
+        >
+      </v-btn>
+
+      <v-btn v-if="link_ig" color="primary" text>
+        <a class="text-decoration-none" :href="link_ig" target="_blank"
+          >Instagram</a
+        >
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    subtitle: {
+      type: String
+    },
+    src: {
+      type: String,
+      required: true
+    },
+    link_website: {
+      type: String
+    },
+    link_github: {
+      type: String
+    },
+    link_ig: {
+      type: String
+    },
+    link_ig_post: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.text-shadow {
+  text-shadow: 0px 0px 5px rgba(50, 50, 50, 1);
+}
+</style>
