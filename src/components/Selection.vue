debe8f3111254d46b32141615c77dd3427d5e478
AfiqRosli <afiq_rosli@live.com>
2021-03-23 00:07:07 +0800

    update cv ver 2021-03-21

--- src/components/Selection.vue
+++ src/components/Selection.vue
@@ -100,7 +100,7 @@ export default {
           new ChatFactory(Chat.TEXT, {
             person: Person.ME,
             msg:
-              "Here you go <a class='text-decoration-none' href='/files/afiq_rosli_software_engineer_cv_2021-03-03.pdf' target='_blank'>Download CV</a>"
+              "Here you go <a class='text-decoration-none' href='/files/afiq_rosli_software_engineer_cv_2021-03-21.pdf' target='_blank'>Download CV</a>"
           })
         );
 
